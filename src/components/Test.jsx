3def2550524d62c7809c47c7079547e015a329d9
David Honeman <david@bakedmediastudios.com>
2025-03-05 15:53:34 -0800

    icon test

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,6 +1,8 @@
+import { SlCup } from "react-icons/sl";
+
 const Test = () => {
     return(
-        <>  
+        <>  <SlCup />
                 <div class="text-2xl" className="header">
             <div class=" bg-blue-700 bg-opacity-50 blur-lg" className="header">
                     <h1 class="relative">Baked Media Studios</h1>
