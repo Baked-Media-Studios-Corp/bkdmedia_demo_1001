a034fc6e4801cda1d1a8512f410ba5e3452d8146
David Honeman <david@bakedmediastudios.com>
2025-03-05 13:45:00 -0800

    1

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Test from './components/Test'
+const App = () => {
+  return (
+    <>
+      <Test />
+    </>
+  )
+}
+export default App
---
+++ src/components/Test.jsx
@@ -0,0 +1,24 @@
+const Test = () => {
+    return(
+        <>  
+                <div class="text-2xl" className="header">
+            <div class=" bg-blue-700 bg-opacity-50 blur-lg" className="header">
+                    <h1 class="relative">Baked Media Studios</h1>
+            </div>
+                    <h2>http://www.bakedmediastudios.com</h2>
+                </div>
+            <div className="documentType">
+
+            </div>
+            <div className="form">
+
+            </div>
+            <div className="footer">
+
+            </div>
+            
+            
+        </>
+    )
+}
+export default Test;
