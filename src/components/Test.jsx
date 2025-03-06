1436541121526c53b94e1c3126e102236882239c
David Honeman <david@bakedmediastudios.com>
2025-03-05 17:35:24 -0800

    Update Test.jsx

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,23 +1,16 @@
 import { SlCup } from "react-icons/sl";
 
 const Test = () => {
-    return(
-        <>  <SlCup />
-                <div class="text-2xl" className="header">
-            <div class=" bg-blue-700 bg-opacity-50 blur-lg" className="header">
+    return (
+        <>  
+            {/* <SlCup /> */}
+            <div class="text-2xl" className="header">
+                <div class=" bg-blue-700 bg-opacity-50 blur-lg" className="header">
                     <h1 class="relative">Baked Media Studios</h1>
-            </div>
-                    <h2>http://www.bakedmediastudios.com</h2>
                 </div>
-            <div className="documentType">
-
+                <h2>http://www.bakedmediastudios.com</h2>
             </div>
-            <div className="form">
-
-            </div>
-            <div className="footer">
-
-            </div>
+            
             
             
         </>
